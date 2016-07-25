27a379473c84a5ce9441e16f44c19242d7cae85f
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2016-07-25 14:35:43 +0100

    Add BirthEventsController

---
+++ app/uk/gov/hmrc/brm/controllers/BirthEventsController.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.brm.controllers
+
+import uk.gov.hmrc.play.microservice.controller.BaseController
+
+/**
+  * Created by chrisianson on 25/07/16.
+  */
+object BirthEventsController extends BaseController {
+
+  def get(args: String) = ???
+}
