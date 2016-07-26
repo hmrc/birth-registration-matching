a71372439417d8c76d39c7cf76bb222fa68f4145
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2016-07-26 09:02:49 +0100

    Add BirthEventsControllerSpec

---
+++ test/uk/gov/hmrc/brm/controllers/BirthEventsControllerSpec.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.brm.controllers
+
+import uk.gov.hmrc.play.test.UnitSpec
+
+/**
+  * Created by chrisianson on 26/07/16.
+  */
+class BirthEventsControllerSpec extends UnitSpec {
+
+  /**
+    * - Should
+    * - GET /api/v0/events/birth should return 200
+    * - GET /api/v0/events/birth should return html
+    * - Return valid JSON response on successful match
+    * - Return valid JSON response on unsuccessful match
+    * - Return response code 500 when API is down
+    * - Return response code 404 when endpoint has been retired and is no longer in use
+    * - Return response code 400 if request contains invalid/missing data
+    * - Return response code 400 if authentication fails
+    * - Return response code 403 if account (service account) is suspended
+   **/
+}
