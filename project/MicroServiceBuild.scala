3932969af1cc8dc25f0367a7479d2bec3f3dc109
Adam Conder <adamdconder@gmail.com>
2016-11-04 10:40:00 +0000

    removing duplicate dependency

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -32,8 +32,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
-    "uk.gov.hmrc" %% "http-verbs" % "6.2.0"
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion
   )
 
   trait TestDependencies {
