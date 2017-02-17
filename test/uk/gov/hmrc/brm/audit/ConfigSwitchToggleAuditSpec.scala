a51358aecd8ed72e003d26765584e458cc18dff7
anujakhare <anuja.khare@digital.hmrc.gov.uk>
2017-02-17 13:13:07 +0000

    BRMS-248:Remove unwanted code

--- test/uk/gov/hmrc/brm/audit/ConfigSwitchToggleAuditSpec.scala
+++ test/uk/gov/hmrc/brm/audit/ConfigSwitchToggleAuditSpec.scala
@@ -21,7 +21,6 @@ import org.scalatest.BeforeAndAfterAll
 import org.joda.time.LocalDate
 import org.mockito.Matchers
 import org.mockito.Mockito._
-import org.scalatest._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.OneAppPerSuite
 import org.specs2.mock.mockito.ArgumentCapture
@@ -33,11 +32,6 @@ import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.UnitSpec
 import play.api.test.Helpers._
-import uk.gov.hmrc.play.audit.model.AuditEvent
-
-import scala.collection.immutable.IndexedSeq
-import scala.concurrent.Future
-
 import scala.concurrent.Future
 
 /**
@@ -49,14 +43,7 @@ class ConfigSwitchToggleAuditSpec extends UnitSpec with MockitoSugar with OneApp
   val auditor = new RequestsAndResultsAudit(connector)
   implicit val hc = HeaderCarrier()
 
-  val auditConfigOnForDefault: Map[String, _] = BaseConfig.config ++ Map(
-    "microservice.services.birth-registration-matching.matching.firstName" -> true,
-    "microservice.services.birth-registration-matching.matching.lastName" -> true,
-    "microservice.services.birth-registration-matching.matching.dateOfBirth" -> true,
-    "microservice.services.birth-registration-matching.matching.matchOnMultiple" -> false,
-    "microservice.services.birth-registration-matching.matching.disableSearchByDetails" -> false,
-    "microservice.services.birth-registration-matching.matching.ignoreMiddleNames" -> true
-  )
+  val auditConfigOnForDefault: Map[String, _] = BaseConfig.config ++ Map()
 
   val auditConfigOnForAlternate: Map[String, _] = BaseConfig.config ++ Map(
     "microservice.services.birth-registration-matching.matching.firstName" -> false,
@@ -66,6 +53,7 @@ class ConfigSwitchToggleAuditSpec extends UnitSpec with MockitoSugar with OneApp
     "microservice.services.birth-registration-matching.matching.disableSearchByDetails" -> true,
     "microservice.services.birth-registration-matching.matching.ignoreMiddleNames" -> false
   )
+
   val auditConfigOnAppForDefault = GuiceApplicationBuilder(disabled = Seq(classOf[com.kenshoo.play.metrics.PlayModule])).configure(auditConfigOnForDefault).build()
   val auditConfigOnAppForAlternate = GuiceApplicationBuilder(disabled = Seq(classOf[com.kenshoo.play.metrics.PlayModule])).configure(auditConfigOnForAlternate).build()
 
@@ -76,7 +64,7 @@ class ConfigSwitchToggleAuditSpec extends UnitSpec with MockitoSugar with OneApp
       auditConfigOnAppForDefault
     ) {
       val event = Map("match" -> "true")
-      val payload = Payload(Some("123456789"), "Adam", "Test", LocalDate.now(), BirthRegisterCountry.ENGLAND)
+      val payload = Payload(Some("123456789"), "Adam", "Test1", LocalDate.now(), BirthRegisterCountry.ENGLAND)
       val argumentCapture = new ArgumentCapture[AuditEvent]
       when(connector.sendEvent(argumentCapture.capture)(Matchers.any(), Matchers.any())).thenReturn(Future.successful(AuditResult.Success))
       val result = await(auditor.audit(event, Some(payload)))
