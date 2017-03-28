5e4084170bad52674aa5e2f5d907de0d80cbabe3
Adam Conder <adamdconder@gmail.com>
2017-03-28 11:07:27 +0100

    BRMS-268: refactoring country audit

--- app/uk/gov/hmrc/brm/audit/WhereBirthRegisteredAudit.scala
+++ app/uk/gov/hmrc/brm/audit/WhereBirthRegisteredAudit.scala
@@ -17,12 +17,15 @@
 package uk.gov.hmrc.brm.audit
 
 import com.google.inject.Singleton
+import play.api.libs.json.{JsDefined, JsValue}
 import uk.gov.hmrc.brm.config.MicroserviceGlobal
 import uk.gov.hmrc.brm.models.brm.Payload
+import uk.gov.hmrc.brm.utils.BirthRegisterCountry
 import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
 import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.Future
+import scala.util.Try
 
 /**
   * Created by adamconder on 09/02/2017.
@@ -40,6 +43,20 @@ class WhereBirthRegisteredAudit(connector: AuditConnector = MicroserviceGlobal.a
 
   def audit(result : Map[String, String], payload: Option[Payload])(implicit hc : HeaderCarrier) : Future[AuditResult] = {
     event(new OtherCountryAuditEvent(result))
+  }
+
+  def auditCountryInRequest(json : JsValue)(implicit hc: HeaderCarrier) = {
+    json.\(Payload.whereBirthRegistered) match {
+      case JsDefined(country) =>
+        Try(BirthRegisterCountry.withName(country.toString)) recover {
+          case _ =>
+            // audit incorrect country
+            audit(Map("country" -> country.toString), None)
+        }
+      case _ =>
+        // does not exist on request
+        audit(Map("country" -> "no country specified"), None)
+    }
   }
 
 }
