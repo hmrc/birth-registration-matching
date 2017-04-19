1072c58d2709df51cd42583e06e90bfdb9202282
Adam Conder <adamdconder@gmail.com>
2017-04-19 13:25:40 +0100

    fix issues with exceptions and upstream exceptions

--- app/uk/gov/hmrc/brm/controllers/BRMBaseController.scala
+++ app/uk/gov/hmrc/brm/controllers/BRMBaseController.scala
@@ -42,18 +42,19 @@ trait BRMBaseController extends BaseController with BRMException {
   def handleException(method: String)(implicit payload: Payload, auditor: BRMAudit, hc: HeaderCarrier): PartialFunction[Throwable, Result] = {
     case t =>
       val allPfs = Seq(
-        notFoundPF(method),
-        badRequestPF(method),
+//        notFoundPF(method),
+//        badRequestPF(method),
         groProxyDownPF(method),
         desConnctionDownPF(method),
         gatewayTimeoutPF(method),
         groConnctionDownPF(method),
         nrsConnctionDownPF(method),
         upstreamErrorPF(method),
+        desInvalidHeadersBadRequestPF(method),
         badRequestExceptionPF(method),
         notImplementedExceptionPF(method),
         notFoundExceptionPF(method),
-        forbiddenPF(method),
+        forbiddenUpstreamPF(method),
         exceptionPF(method)).reduce(_ orElse _)
 
       // audit the transaction when there was an exception with default arguments
