7ab5c48ff839bbf32cdfcb0ef7817421e42fa308
Manish Wadhwani <manish.wadhwani@digital.hmrc.gov.uk>
2017-05-09 10:39:48 +0100

    audit only for request and results

--- app/uk/gov/hmrc/brm/controllers/BirthEventsController.scala
+++ app/uk/gov/hmrc/brm/controllers/BirthEventsController.scala
@@ -69,7 +69,7 @@ trait BirthEventsController extends HeaderValidator with BRMBaseController {
               //TODO: 1. What We're going to log
               //TODO: 2. Do we want to do any auditing within the FeatureFactory
               //TODO: 3. Re-insert auditTransaction (see method below)
-              // auditTransaction()
+              auditRequestAndResults()
               info(CLASS_NAME, "post()", s"date of birth is before valid date / search is switched off (all/reference/details)")
               Future.successful(respond(Ok(Json.toJson(BirthResponseBuilder.withNoMatch()))))
             }
