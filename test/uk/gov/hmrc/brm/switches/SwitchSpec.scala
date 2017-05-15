b3b3ff23666341f6bf39e06095be2f6b8ccff0f6
Adam Conder <adamdconder@gmail.com>
2017-05-15 12:13:46 +0100

    Refactor: adding tests:

--- test/uk/gov/hmrc/brm/switches/SwitchSpec.scala
+++ test/uk/gov/hmrc/brm/switches/SwitchSpec.scala
@@ -30,6 +30,10 @@ class SwitchSpec extends UnitSpec with BeforeAndAfter with OneAppPerTest {
     override val name = "test"
   }
 
+  object NonExistingSwitch extends Switch {
+    override val name = "invalid"
+  }
+
   def switchDisabled: Map[String, _] = Map(
     "microservice.services.birth-registration-matching.features.test.enabled" -> false
   )
@@ -46,7 +50,7 @@ class SwitchSpec extends UnitSpec with BeforeAndAfter with OneAppPerTest {
     } else if (testData.tags.contains("disabled")) {
       switchDisabled
     } else {
-      throw new Exception("cannot create correct test configuration")
+      Map.empty[String, Any]
     }
   }.build()
 
@@ -60,6 +64,70 @@ class SwitchSpec extends UnitSpec with BeforeAndAfter with OneAppPerTest {
     "load configuration for a feature and return false for isEnabled" taggedAs Tag("disabled") in {
       val switch = TestSwitch
       switch.isEnabled shouldBe false
+    }
+
+    "throw FeatureSwitchException for configuration that doesn't exist" in {
+      intercept[RuntimeException](
+        NonExistingSwitch.isEnabled
+      )
+    }
+
+  }
+
+  "GRO" should {
+
+    "be enabled for GRO" in {
+      GROSwitch.isEnabled shouldBe true
+    }
+
+    "be enabled for GRO Reference" in {
+      GROReferenceSwitch.isEnabled shouldBe true
+    }
+
+    "be enabled for GRO Details" in {
+      GRODetailsSwitch.isEnabled shouldBe true
+    }
+
+  }
+
+//  "NRS" should {
+//
+//    "be enabled for NRS" in {
+//      NRSSwitch.isEnabled shouldBe true
+//    }
+//
+//    "be enabled for NRS Reference" in {
+//      NRSReferenceSwitch.isEnabled shouldBe true
+//    }
+//
+//    "be enabled for NRS Details" in {
+//      NRSDetailsSwitch.isEnabled shouldBe true
+//    }
+//
+//  }
+//
+//  "GRO-NI" should {
+//    "be enabled for GRO-NI" in {
+//      GRONISwitch.isEnabled shouldBe true
+//    }
+//
+//    "be enabled for GRO-NI Reference" in {
+//      GRONIReferenceSwitch.isEnabled shouldBe true
+//    }
+//
+//    "be enabled for GRO-NI Details" in {
+//      GRONIDetailsSwitch.isEnabled shouldBe true
+//    }
+//  }
+
+  "DateOfBirth" should {
+
+    "be enabled" in {
+      DateOfBirthSwitch.isEnabled shouldBe true
+    }
+
+    "have a value" in {
+      DateOfBirthSwitchValue.value should not be empty
     }
 
   }
