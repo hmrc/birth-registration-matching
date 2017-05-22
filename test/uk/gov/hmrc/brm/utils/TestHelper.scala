dd7d78f8526f5c34bfca0349c203d620cb48bb23
Manish Wadhwani <manish.wadhwani@digital.hmrc.gov.uk>
2017-05-22 12:06:12 +0100

    updated with more tests

--- test/uk/gov/hmrc/brm/utils/TestHelper.scala
+++ test/uk/gov/hmrc/brm/utils/TestHelper.scala
@@ -42,6 +42,7 @@ object TestHelper {
   val groResponseWithoutAdditionalName = JsonUtils.getJsonFromFile("gro","without_additional_name")
   val groResponseWithMoreAdditionalName = JsonUtils.getJsonFromFile("gro","with_more_additional_name")
   val groResponseWithSpecialCharacter = JsonUtils.getJsonFromFile("gro","with_special_character")
+  val groResponse500036682 = JsonUtils.getJsonFromFile("gro","500036682")
 
 
   val payload = Payload(Some("500035710"), "Adam", None, "Wilson", new LocalDate("2006-11-12"), BirthRegisterCountry.ENGLAND)
