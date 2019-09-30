7d3e102ab78959910d6f3c078d2bf6e5d51a8895
shikkhasingh <shika.singh@digital.hmrc.gov.uk>
2019-09-30 14:37:31 +0100

    DDCNOS-5999 | adding logger for custom dashboard

--- app/uk/gov/hmrc/brm/connectors/BirthConnector.scala
+++ app/uk/gov/hmrc/brm/connectors/BirthConnector.scala
@@ -75,7 +75,7 @@ trait BirthConnector extends ServicesConfig {
     BRMLogger.debug("BirthConnector", "sendRequest", s"[Request]: $request [HeaderCarrier withExtraHeaders]: $newHc")
 
     response.onComplete(r => {
-      BRMLogger.warn("BirthConnector", "sendRequest", s"[HttpResponse]: [status] ${r.map(_.status)}")
+      BRMLogger.warn("BirthConnector", "sendRequest", s"[HttpResponse]: ${r.map(_.status)}")
       BRMLogger.debug("BirthConnector", "sendRequest", s"[HttpResponse]: [status] ${r.map(_.status)} [body] ${r.map(_.body)} [headers] ${r.map(_.allHeaders)}")
     }
     )
