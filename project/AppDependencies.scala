576f72969010d0a424729b15a34ae9c530d57895
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-01-26 10:42:34 +0000

    DDCE-5057: Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import play.sbt.PlayImport.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "7.22.0"
+  private val bootstrapPlayVersion = "7.23.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "com.typesafe.play" %% "play-json-joda"            % "2.9.4"
+    "com.typesafe.play" %% "play-json-joda"            % "2.10.4"
   )
 
   val test: Seq[ModuleID]    = Seq(
     "org.scalatest"       %% "scalatest"              % "3.2.17",
     "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootstrapPlayVersion,
     "org.scalatestplus"   %% "mockito-3-4"            % "3.2.10.0",
-    "org.specs2"          %% "specs2-core"            % "4.20.3",
-    "org.specs2"          %% "specs2-mock"            % "4.20.3",
+    "org.specs2"          %% "specs2-core"            % "4.20.5",
+    "org.specs2"          %% "specs2-mock"            % "4.20.5",
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8"
   ).map(_ % "test")
 
