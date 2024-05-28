197120ed5cc88dc4db647e98171fbe995fd400d3
Danny Pennington <63240616+DannyPennington97@users.noreply.github.com>
2024-05-28 15:42:55 +0100

    [DDCE-5530] Re-implement min length on additionalNames field (#166)

--- test/uk/gov/hmrc/brm/controllers/BirthEventsControllerAdditionalNameSwitchSpec.scala
+++ test/uk/gov/hmrc/brm/controllers/BirthEventsControllerAdditionalNameSwitchSpec.scala
@@ -429,11 +429,11 @@ class BirthEventsControllerAdditionalNameSwitchSpec
 
   "validate additionalNames when ignoreAdditionalName is false." should {
 
-    "return response code 200 if request contains additionalName key but no value" in {
+    "return response code 400 if request contains additionalName key but no value" in {
       mockAuditSuccess
       val request = postRequest(additionalNamesKeyNoValue)
       val result  = testController.post().apply(request).futureValue
-      checkResponse(result, OK, matchResponse = false)
+      checkResponse(result, BAD_REQUEST, MockErrorResponses.INVALID_ADDITIONALNAMES.json)
     }
 
     "return response code 400 if request contains special characters in additionalName" in {
